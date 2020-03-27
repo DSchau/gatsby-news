a01ef1bc3972f22be2aacf603dc3c32ef78798c2
Dustin Schau <dustinschau@gmail.com>
2020-03-26 22:43:53 -0700

    chore: various tweaks

--- functions/seo/seo.js
+++ functions/seo/seo.js
@@ -1,5 +1,9 @@
 const qs = require('querystring')
 
+/*
+ * TODO: Render an SEO graphic
+ * Cloudinary? Maybe.
+ */
 module.exports = async event => {
   const params = qs.parse(event.body)
   return {
