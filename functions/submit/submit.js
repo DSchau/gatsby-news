b92aa984b1bcf412b8e5bf00c7b35ea72eeb28c1
Dustin Schau <dustinschau@gmail.com>
2020-03-28 10:58:32 -0700

    chore: deploy

--- functions/submit/submit.js
+++ functions/submit/submit.js
@@ -1,7 +1,30 @@
+const yup = require('yup')
+
+const schema = yup.object().shape({
+  title: yup.string().required(),
+  excerpt: yup.string(),
+  content: yup.string().required(),
+  author: yup.string().required(),
+  categories: yup.array().of(yup.string()),
+  tags: yup.array().of(yup.string())
+})
+
 exports.handler = async event => {
-  const params = qs.parse(event.body)
+  let body
+  try {
+    body = await schema.validate(JSON.parse(event.body))
+  } catch (e) {
+    return {
+      status: 500,
+      body: JSON.stringify({
+        message: e.message,
+        stack: e.stack
+      })
+    }
+  }
+
   return {
     status: 200,
-    body: JSON.stringify(params)
+    body: JSON.stringify(body)
   }
 }
