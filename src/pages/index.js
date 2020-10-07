f777649796f0ac552544fe9b3ac8831f74c5ce17
Dustin Schau <dustinschau@gmail.com>
2020-10-07 09:10:02 -0700

    chore: add date

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = props => <Page {...props} />
 
 export const pageQuery = graphql`
   query IndexQuery {
-    posts: allWpPost(filter: {categories: {nodes: {elemMatch: {name: {eq: "Latest News"}}}}}) {
+    posts: allWpPost(sort: {fields: date, order:DESC}, filter: {categories: {nodes: {elemMatch: {name: {eq: "Latest News"}}}}}) {
       nodes {
         ...WpPostPreviewFragment
       }
--- src/templates/today.js
+++ src/templates/today.js
@@ -7,7 +7,7 @@ const TodayPage = props => <Page {...props} />
 
 export const pageQuery = graphql`
   query TodayQuery($date: Date!) {
-    posts: allWpPost(limit:25, filter: {date: { eq: $date }, categories: {nodes: {elemMatch: {name: {eq: "Latest News"}}}}}) {
+    posts: allWpPost(limit:25, sort: {fields: date, order:DESC}, filter: {date: { eq: $date }, categories: {nodes: {elemMatch: {name: {eq: "Latest News"}}}}}) {
       nodes {
         ...WpPostPreviewFragment
       }
